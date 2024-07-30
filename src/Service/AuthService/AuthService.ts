b3796bc89752700b4958f5c3caf32d820c98c66a
Lucas Chicoski dos Santos <lucaschicoskidossantos@MacBook-Air-de-Lucas.local>
2024-07-30 01:06:36 -0300

    ajuste

--- src/Service/AuthService/AuthService.ts
+++ src/Service/AuthService/AuthService.ts
@@ -1,15 +1,23 @@
 import { RepositoryFactory, RepositoryType } from "../../Global/IOC";
 import { IAuthService } from "./IAuthService";
-import { AuthRepository } from "src/Infrastructure/Repository/AuthRepository/AuthRepository";
+import { AuthRepository } from "../../Infrastructure/Repository/AuthRepository/AuthRepository";
 import { User } from "@prisma/client";
 import { UserModel } from "../../Domain/Models/UserModel";
 
 
 
 export class AuthService implements IAuthService {
-    async login(user_code: string): Promise<UserModel> {
+    async login(user_code: string): Promise<any> {
         const authRepository: AuthRepository = RepositoryFactory.getRepository(RepositoryType.AuthRepository)
         const result: User = await authRepository.login(user_code)
+
+        if(!result){
+            return {
+                response: null,
+                message: "nenhum usuário com esse código"
+            }
+        }
+
         const userModel = new UserModel({ code_user: result.code_user_id, name_user: result.nameUser })
         return userModel as UserModel
     }
